971e3dabf55f7db69288f4702d41321f9a14a979
hardwarehuman <github@klasser.net>
2017-03-05 20:16:36 -0800

    UserID translation, bugfix on re-parses, query performance imp.

--- preprocessor/slack_parser.sh
+++ preprocessor/slack_parser.sh
@@ -2,7 +2,6 @@
 
 scriptname=$0
 indirs=$@
-msg_to_csv='$1=="\"user\":" {split($2,a,"\""); user=a[2]}$1=="\"text\":" {split($0,a,": "); text=a[2]; sub(/,$/,"",text)}$1=="\"ts\":" {split($2,a,"\""); timestamp=a[2]; print timestamp","user","text}'
 if [ "$indirs " == " " ]
 then
   echo "Usage: $scriptname directory_list"
@@ -29,8 +28,20 @@ then
   exit 2
 fi
 
+interleave_day(){
+  ye=${1}
+  shift
+  mo=${1}
+  shift
+  da=${1}
+  shift
+  fs=$@
+  cat ${fs} | sort > ${outdir}/${ye}-${mo}-${da}.csv
+}
+
 interleave_days(){
   lyear=2017
+  # No slack archives before its creation in 2009
   for year in $(seq 2009 $lyear)
   do
     for month in $(seq -w 01 12)
@@ -39,23 +50,31 @@ interleave_days(){
       do
         files=`find $outdir -name "${year}-${month}-${day}:*.csv"`
         if [ "$files " != " " ]; then
-          cat $files |sort > ${outdir}/${year}-${month}-${day}.csv
+          interleave_day ${year} ${month} ${day} ${files}
+          #cat $files | sort > ${outdir}/${year}-${month}-${day}.csv
         fi
       done
     done
   done
 }
 timestamper
+user_translation=""
 for indir in $indirs
 do
   if [ -d $indir ] && [ -r $indir ]
   then
+    # this only works because find returns shallower directories first
+    # if retained, the username translation logic should move to its own loop
+    if [ -f "$indir/users.json" ]
+    then # setting user ID to username translations
+      seds=`awk -F\" '/\"id\":/{uid=$4} /\"name\":/{seds=seds"; s/"uid"/"$4"/g"} END{print substr(seds,3)}' $indir/users.json`
+    fi
     for dayfile in `ls $indir/*.json`
     do
       dayfile=`basename $dayfile`
       channel_name=`echo $indir | awk '{dirdepth=split($0,a,"/"); print a[dirdepth-1]":"a[dirdepth]}'`
       echo "" > $outdir/${dayfile%.json}:$channel_name.csv #clear out anything that's there w/o needing to delete
-      grep -A 4 "\"type\": \"message\"," $indir/$dayfile | awk -v src_chan=$channel_name '$1=="\"user\":" {split($2,a,"\""); user=a[2]}$1=="\"text\":" {split($0,a,": "); text=a[2]; sub(/,$/,"",text)}$1=="\"ts\":" {split($2,a,"\""); timestamp=a[2]; print timestamp","user","src_chan","text}' >> $outdir/${dayfile%.json}:$channel_name.csv
+      grep -A 4 "\"type\": \"message\"," $indir/$dayfile |sed "${seds}" | awk -v src_chan=$channel_name '$1=="\"user\":" {split($2,a,"\""); user=a[2]}$1=="\"text\":" {split($0,a,": "); text=a[2]; sub(/,$/,"",text)}$1=="\"ts\":" {split($2,a,"\""); timestamp=a[2]; print timestamp","user","src_chan","text}' >> $outdir/${dayfile%.json}:$channel_name.csv
     done
   else
     echo "$indir is not a readable directory;skipping"
